1a50ac69d650eb1a388af8dac680063ea523afee
RaviGarbujaPun <cbpmgr@gmail.com>
2017-12-22 17:13:06 +0545

    Add POJO class for Contact

---
+++ app/src/main/java/np/com/ravi/phonebookpermisson/Contact.java
@@ -0,0 +1,33 @@
+package np.com.ravi.phonebookpermisson;
+
+/**
+ * Created by ravi on 12/22/17.
+ */
+
+public class Contact {
+    //POJO
+    String name;
+    String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+//        return super.toString();
+        return (this.name+"\n"+this.phoneNumber);
+    }
+}
